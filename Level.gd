53fce50c73a52df130b0b875c1898569c359c67c
Agnieszka K. Kocinski <0robotzero@gmail.com>
2021-07-05 21:30:02 +0100

    level, debug camera, lines

--- Level.gd
+++ Level.gd
@@ -2,38 +2,36 @@ extends Node2D
 
 var topTexture: Texture = preload("res://textures/rope/line_top.png")
 var bottomTexture: Texture = preload("res://textures/rope/line_bottom.png")
+var endTexture: Texture = preload("res://textures/rope/line_end.png")
 
 func _ready():
-	$Bottom1.isBottom = true
-	$Bottom2.isBottom = true
-	$Bottom3.isBottom = true
-	
-	for child in $Top1.get_children():
+	for child in $End.get_children():
 		if child is Sprite:
 			var ch := child as Sprite
-			ch.set_texture(topTexture)
+			ch.set_texture(endTexture)
 			
-	for child in $Top2.get_children():
-		if child is Sprite:
-			var ch := child as Sprite
-			ch.set_texture(topTexture)
-			
-	for child in $Top3.get_children():
-		if child is Sprite:
-			var ch := child as Sprite
-			ch.set_texture(topTexture)
-			
-	for child in $Bottom1.get_children():
-		if child is Sprite:
-			var ch := child as Sprite
-			ch.set_texture(bottomTexture)
-			
-	for child in $Bottom2.get_children():
-		if child is Sprite:
-			var ch := child as Sprite
-			ch.set_texture(bottomTexture)
-			
-	for child in $Bottom3.get_children():
-		if child is Sprite:
-			var ch := child as Sprite
-			ch.set_texture(bottomTexture)
+	for n in range (1, 25):
+		var nameTop = "Top%d" % n
+		var nameBottom = "Bottom%d" % n
+		var currentNodeTop = get_node(nameTop)
+		var currentNodeBottom = get_node(nameBottom)
+		if currentNodeTop:
+			for child in currentNodeTop.get_children():
+				if child is Sprite:
+					var ch := child as Sprite
+					ch.set_texture(topTexture)
+		
+		if currentNodeBottom:
+			for child in currentNodeBottom.get_children():
+				if child is Sprite:
+					var ch := child as Sprite
+					ch.set_texture(bottomTexture)
+			currentNodeBottom.isBottom = true
+		
+func _process(delta):
+	if Input.is_action_just_pressed("cameraDebug"):
+		if $Fred.get_node("FredCamera").current:
+			$DebugCamera.current = true
+		else:
+			$Fred.get_node("FredCamera").current = true
+		
